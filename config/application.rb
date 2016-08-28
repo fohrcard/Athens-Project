6554df48cedca79952a46cfcd100ebb37866dffa
Cameron Barker <barker.cameron@gmail.com>
2016-08-28 08:46:43 -0700

    adding generators for requests

--- config/application.rb
+++ config/application.rb
@@ -26,9 +26,9 @@ module RailsApi
         fixtures: false,
         view_specs: false,
         helper_specs: false,
-        routing_specs: false,
-        controller_specs: false,
-        request_specs: false
+        routing_specs: true,
+        controller_specs: true,
+        request_specs: true
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
 

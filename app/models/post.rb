bfc8d72529a792642fbcc9587b53edd1a4a75260
Cameron Barker <barker.cameron@gmail.com>
2016-09-03 23:19:30 -0400

    adding post vote count

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,17 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :votes
+
+  # Benchmark count vs ruby select across all vote counts
+  # db: @real=0.027787707003881223
+  # ruby: @real=0.24628183399909176
+  def total_votes
+    votes.count
+  end
+  def image_one_total_votes
+    votes.where(image_one: true).count
+  end
+  def image_two_total_votes
+    votes.where(image_one: false).count
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -14,4 +14,18 @@ RSpec.describe Post, type: :model do
     user_two.votes.create(post_id: post.id, image_one: true)
     expect(post.votes.size).to eq(2)
   end
+
+  it "shows the votes breakdown" do
+    user = create(:user)
+    post = create(:post, user_id: user.id)
+
+    Vote.create(user_id: create(:user).id, post_id: post.id, image_one: true)
+    Vote.create(user_id: create(:user).id, post_id: post.id, image_one: true)
+    Vote.create(user_id: create(:user).id, post_id: post.id, image_one: false)
+
+
+    expect(post.total_votes).to eq(3)
+    expect(post.image_one_total_votes).to eq(2)
+    expect(post.image_two_total_votes).to eq(1)
+  end
 end

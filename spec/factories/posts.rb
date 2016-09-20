cbf6eb43942d7120bf61e948d07af913b9c7219b
Cameron Barker <barker.cameron@gmail.com>
2016-09-20 16:54:52 -0400

    updating factory with images of the correct ration

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :post do
     user nil
-    image_one {Faker::Avatar.image("image_one")}
-    image_two {Faker::Avatar.image("image_two")}
+    image_one {%w(https://unsplash.it/1080/1080/?random https://unsplash.it/1080/1350/?random https://unsplash.it/1080/566/?random).sample}
+    image_two {%w(https://unsplash.it/1080/1080/?random https://unsplash.it/1080/1350/?random https://unsplash.it/1080/566/?random).sample}
   end
 end
